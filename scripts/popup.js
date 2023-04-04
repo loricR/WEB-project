9c7505bf5b0a02a3e118c658d2ea3e6e7e53d0d6
LOLO\loric <loric.ravassard@laposte.net>
2023-04-04 17:20:32 +0200

    formulaire en popup

---
+++ scripts/popup.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", function init() {
+    this.document.getElementById("show-login-btn").addEventListener("click", function () {  //event pour afficher le formulaire de connexion
+        showPopup("popup-login")
+    });
+    this.document.getElementById("show-login-link").addEventListener("click", function () {  //event pour afficher le formulaire de connexion
+        showPopup("popup-login")
+    });
+    this.document.getElementById("close-login-btn").addEventListener("click", function () { //event pour fermer le formulaire de connexion
+        hidePopup("popup-login")
+    });
+    /*this.document.getElementById("show-register-btn").addEventListener("click", function () {   //event pour afficher le formulaire d'inscription
+        showPopup("popup-register")
+    });*/
+
+    //marche pas
+    this.document.getElementById("show-register-link").addEventListener("click", function () {  //event pour fermer le formulaire d'inscription
+        showPopup("popup-register")
+    });
+
+    //marche pas
+    this.document.getElementById("close-register-btn").addEventListener("click", function () {  //event pour fermer le formulaire d'inscription
+        hidePopup("popup-register")
+    });
+})
+
+function showPopup(objectName) {
+    var objects = document.getElementsByClassName("popup");
+    Array.from(objects).forEach((obj) => {
+        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichées)
+    });
+    document.getElementById(objectName).classList.add("active");    //Ajout de la classe active pour que le formulaire s'affiche (modification dans le css)
+}
+
+function hidePopup(objectName) {
+    var objects = document.getElementsByClassName("popup");
+    Array.from(objects).forEach((obj) => {
+        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichées)
+    });
+    document.getElementById(objectName).classList.remove("active"); //Suppression de la classe active pour que le formulaire se ferme (se cache avec modification dans le css)
+}
