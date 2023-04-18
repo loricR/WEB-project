e67ab2d9ab2365c005df854b5265617a4294ca58
LORIC\loric <loric.ravassard@gmail.com>
2023-04-18 16:52:37 +0200

    Conversion utf-8

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function init() {
-    if (!document.cookie.split(";").some((item) => item.trim().startsWith("login="))) { //Si le cookie de connexion n'existe pas (donc l'utilisateur n'est pas connecté)
+    if (!document.cookie.split(";").some((item) => item.trim().startsWith("login="))) { //Si le cookie de connexion n'existe pas (donc l'utilisateur n'est pas connectÃ©)
         this.document.getElementById("show-login-btn").addEventListener("click", function () {  //event pour afficher le formulaire de connexion
             showPopup("popup-login")
         });
@@ -29,7 +29,7 @@ window.addEventListener("load", function init() {
 function showPopup(objectName) {
     var objects = document.getElementsByClassName("popup");
     Array.from(objects).forEach((obj) => {
-        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichées)
+        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichÃ©es)
     });
     document.getElementById(objectName).classList.add("active");    //Ajout de la classe active pour que le formulaire s'affiche (modification dans le css)
 }
@@ -37,7 +37,7 @@ function showPopup(objectName) {
 function hidePopup(objectName) {
     var objects = document.getElementsByClassName("popup");
     Array.from(objects).forEach((obj) => {
-        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichées)
+        obj.classList.remove("active"); //Suppression du active sur tous les autres objets (pour pas avoir plusieurs popup affichÃ©es)
     });
     document.getElementById(objectName).classList.remove("active"); //Suppression de la classe active pour que le formulaire se ferme (se cache avec modification dans le css)
 }
